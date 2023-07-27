798531ad0bf70bcb1da0d96b7916aeef018fe636
george.smith <litesoft@gmail.com>
2023-07-27 09:45:28 -0700

    Tests and some generic usage for shared behavior!

--- check.go
+++ check.go
@@ -2,27 +2,40 @@ package go_utils_check
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
 )
 
 var TestIndent = "             "
 
+// IsNil -- checks 'any' thing to determine if it is directly 'nil' or
+// indirectly 'nil': Interface(s), Slice(s), Map(s), Func(s), & Ptr(s)
+// -
 func IsNil(it any) bool {
 	if it == nil {
 		return true
 	}
-	itrv := reflect.ValueOf(it)
-	if !itrv.IsValid() {
+	reflectValue := reflect.ValueOf(it)
+	if !reflectValue.IsValid() {
 		return true
 	}
-	switch itrv.Kind() {
-	case reflect.Ptr, reflect.Slice, reflect.Map, reflect.Func, reflect.Interface:
-		return itrv.IsNil()
+	switch reflectValue.Kind() {
+	case reflect.Interface, reflect.Slice, reflect.Map, reflect.Func, reflect.Ptr: // Order I think is most likely!
+		return reflectValue.IsNil()
 	default:
 		return false
 	}
 }
 
+// StringVisibleAsciiOrNonAsciiUTF8 -- implements a check of the passed in string to (simplistically) enforce
+// that it consists of only visible characters.  It errors on the following conditions:
+// * string is empty
+// * any rune in the string is:
+// ** <= ' '
+// ** == 127 (DEL)
+//
+// Known simplistic aspect is that there is no checking for non-ascii whitespace!
+// -
 func StringVisibleAsciiOrNonAsciiUTF8(str string) error {
 	if str == "" {
 		return errors.New("was empty")
@@ -37,6 +50,38 @@ func StringVisibleAsciiOrNonAsciiUTF8(str string) error {
 		if r == 127 {
 			return errors.New("contains the non-visible 'DEL' (127) ascii character")
 		}
+	}
+	return nil
+}
+
+// LimitIntRange -- determines if any kind of signed integer is 'min' <= 'value' <= 'max' (inclusive)
+// Note: 'min' and 'max' are 'int64's
+// -
+func LimitIntRange[T int64 | int32 | int16 | int8 | int](value T, what string, min, max int64) (T, error) {
+	err := checkRange(int64(value), what, min, max)
+	if err != nil {
+		value = 0
+	}
+	return value, err
+}
+
+// LimitUintRange -- determines if any kind of unsigned integer is 'min' <= 'value' <= 'max' (inclusive)
+// Note: 'min' and 'max' are 'uint64's
+// -
+func LimitUintRange[T uint64 | uint32 | uint16 | uint8 | uint](value T, what string, min, max uint64) (T, error) {
+	err := checkRange(uint64(value), what, min, max)
+	if err != nil {
+		value = 0
+	}
+	return value, err
+}
+
+func checkRange[T int64 | uint64](value T, what string, min, max T) error {
+	if value < min {
+		return fmt.Errorf("value (%v < %v) minimum for: %v", value, min, what)
+	}
+	if max < value {
+		return fmt.Errorf("value (%v > %v) maximum for: %v", value, max, what)
 	}
 	return nil
 }
--- check_test.go
+++ check_test.go
@@ -2,6 +2,7 @@ package go_utils_check
 
 import (
 	"fmt"
+	"math"
 	"testing"
 )
 
@@ -67,4 +68,223 @@ func Test_StringVisibleAsciiOrNonAsciiUTF8(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_LimitIntRange_int64(t *testing.T) {
+	tests := []struct {
+		value    int64
+		min, max int64
+		wantErr  bool
+	}{
+		{-1, 0, 5, true},
+		{-1, -1, 5, false},
+		{-1, -1, 1, false},
+	}
+	checkLimitIntRangeMaxMin[int](t, math.MinInt64, math.MaxInt64)
+	for _, tt := range tests {
+		checkLimitIntRange(t, tt.wantErr, tt.value, tt.min, tt.max)
+	}
+}
+
+func Test_LimitIntRange_int32(t *testing.T) {
+	tests := []struct {
+		value    int32
+		min, max int64
+		wantErr  bool
+	}{
+		{-1, 0, 5, true},
+		{-1, -1, 5, false},
+		{-1, -1, 1, false},
+	}
+	checkLimitIntRangeMaxMin[int](t, math.MinInt32, math.MaxInt32)
+	for _, tt := range tests {
+		checkLimitIntRange(t, tt.wantErr, tt.value, tt.min, tt.max)
+	}
+}
+
+func Test_LimitIntRange_int16(t *testing.T) {
+	tests := []struct {
+		value    int16
+		min, max int64
+		wantErr  bool
+	}{
+		{-1, 0, 5, true},
+		{-1, -1, 5, false},
+		{-1, -1, 1, false},
+	}
+	checkLimitIntRangeMaxMin[int](t, math.MinInt16, math.MaxInt16)
+	for _, tt := range tests {
+		checkLimitIntRange(t, tt.wantErr, tt.value, tt.min, tt.max)
+	}
+}
+
+func Test_LimitIntRange_int8(t *testing.T) {
+	tests := []struct {
+		value    int8
+		min, max int64
+		wantErr  bool
+	}{
+		{-1, 0, 5, true},
+		{-1, -1, 5, false},
+		{-1, -1, 1, false},
+	}
+	checkLimitIntRangeMaxMin[int](t, math.MinInt8, math.MaxInt8)
+	for _, tt := range tests {
+		checkLimitIntRange(t, tt.wantErr, tt.value, tt.min, tt.max)
+	}
+}
+
+func Test_LimitIntRange_int(t *testing.T) {
+	tests := []struct {
+		value    int
+		min, max int64
+		wantErr  bool
+	}{
+		{-1, 0, 5, true},
+		{-1, -1, 5, false},
+		{-1, -1, 1, false},
+	}
+	checkLimitIntRangeMaxMin[int](t, math.MinInt, math.MaxInt)
+	for _, tt := range tests {
+		checkLimitIntRange(t, tt.wantErr, tt.value, tt.min, tt.max)
+	}
+}
+
+func checkLimitIntRangeMaxMin[T int64 | int32 | int16 | int8 | int](t *testing.T, min, max T) {
+	min64, max64 := int64(min), int64(max)
+	checkLimitIntRange(t, false, min, min64, max64)
+	checkLimitIntRange(t, false, max, min64, max64)
+	checkLimitIntRange(t, true, min, min64+1, max64)
+	checkLimitIntRange(t, true, max, min64, max64-1)
+
+}
+
+func checkLimitIntRange[T int64 | int32 | int16 | int8 | int](t *testing.T, wantErr bool, inputValue T, min, max int64) {
+	name := fmt.Sprintf("(%v,%v,%v)", inputValue, min, max)
+	t.Run(name, func(t *testing.T) {
+		actualValue, err := LimitIntRange(inputValue, name, min, max)
+		if (err != nil) != wantErr {
+			t.Errorf("LimitIntRange() error = %v, wantErr %v", err, wantErr)
+		} else {
+			expectedValue := inputValue
+			if err != nil {
+				fmt.Println(TestIndent, "expected error:", name, "--", err)
+				expectedValue = 0
+			}
+			if actualValue != expectedValue {
+				t.Errorf("LimitIntRange() expectedValue (%v != %v) actualValue", expectedValue, actualValue)
+			}
+		}
+	})
+
+}
+
+// ////////////////////////////////////////////////////////////////////////////////////////////
+func Test_LimitUintRange_uint64(t *testing.T) {
+	tests := []struct {
+		value    uint64
+		min, max uint64
+		wantErr  bool
+	}{
+		{0, 1, 5, true},
+		{0, 0, 5, false},
+		{1, 0, 1, false},
+	}
+	checkLimitUintRangeMaxMin[uint](t, 0, math.MaxUint64)
+	for _, tt := range tests {
+		checkLimitUintRange(t, tt.wantErr, tt.value, tt.min, tt.max)
+	}
+}
+
+func Test_LimitUintRange_uint32(t *testing.T) {
+	tests := []struct {
+		value    uint32
+		min, max uint64
+		wantErr  bool
+	}{
+		{0, 1, 5, true},
+		{0, 0, 5, false},
+		{1, 0, 1, false},
+	}
+	checkLimitUintRangeMaxMin[uint](t, 0, math.MaxUint32)
+	for _, tt := range tests {
+		checkLimitUintRange(t, tt.wantErr, tt.value, tt.min, tt.max)
+	}
+}
+
+func Test_LimitUintRange_uint16(t *testing.T) {
+	tests := []struct {
+		value    uint16
+		min, max uint64
+		wantErr  bool
+	}{
+		{0, 1, 5, true},
+		{0, 0, 5, false},
+		{1, 0, 1, false},
+	}
+	checkLimitUintRangeMaxMin[uint](t, 0, math.MaxUint16)
+	for _, tt := range tests {
+		checkLimitUintRange(t, tt.wantErr, tt.value, tt.min, tt.max)
+	}
+}
+
+func Test_LimitUintRange_uint8(t *testing.T) {
+	tests := []struct {
+		value    uint8
+		min, max uint64
+		wantErr  bool
+	}{
+		{0, 1, 5, true},
+		{0, 0, 5, false},
+		{1, 0, 1, false},
+	}
+	checkLimitUintRangeMaxMin[uint](t, 0, math.MaxUint8)
+	for _, tt := range tests {
+		checkLimitUintRange(t, tt.wantErr, tt.value, tt.min, tt.max)
+	}
+}
+
+func Test_LimitUintRange_uint(t *testing.T) {
+	tests := []struct {
+		value    uint
+		min, max uint64
+		wantErr  bool
+	}{
+		{0, 1, 5, true},
+		{0, 0, 5, false},
+		{1, 0, 1, false},
+	}
+	checkLimitUintRangeMaxMin[uint](t, 0, math.MaxUint)
+	for _, tt := range tests {
+		checkLimitUintRange(t, tt.wantErr, tt.value, tt.min, tt.max)
+	}
+}
+
+func checkLimitUintRangeMaxMin[T uint64 | uint32 | uint16 | uint8 | uint](t *testing.T, min, max T) {
+	min64, max64 := uint64(min), uint64(max)
+	checkLimitUintRange(t, false, min, min64, max64)
+	checkLimitUintRange(t, false, max, min64, max64)
+	checkLimitUintRange(t, true, min, min64+1, max64)
+	checkLimitUintRange(t, true, max, min64, max64-1)
+
+}
+
+func checkLimitUintRange[T uint64 | uint32 | uint16 | uint8 | uint](t *testing.T, wantErr bool, inputValue T, min, max uint64) {
+	name := fmt.Sprintf("(%v,%v,%v)", inputValue, min, max)
+	t.Run(name, func(t *testing.T) {
+		actualValue, err := LimitUintRange(inputValue, name, min, max)
+		if (err != nil) != wantErr {
+			t.Errorf("LimitUintRange() error = %v, wantErr %v", err, wantErr)
+		} else {
+			expectedValue := inputValue
+			if err != nil {
+				fmt.Println(TestIndent, "expected error:", name, "--", err)
+				expectedValue = 0
+			}
+			if actualValue != expectedValue {
+				t.Errorf("LimitUintRange() expectedValue (%v != %v) actualValue", expectedValue, actualValue)
+			}
+		}
+	})
+
 }
